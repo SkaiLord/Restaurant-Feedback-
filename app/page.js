fc337cf19d4c0749bb0605a0a4d560511b7f38d6
SkaiLord <skailord007@gmail.com>
2024-02-23 12:49:32 +0530

    first draft

--- app/page.js
+++ app/page.js
@@ -1,112 +1,222 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import logo from '../public/reviews-logo.png';
+import {
+  BsEmojiAngry,
+  BsEmojiFrown,
+  BsEmojiGrin,
+  BsEmojiNeutral,
+  BsEmojiSmile,
+} from 'react-icons/bs';
 
 export default function Home() {
+  const [foodRating, setFoodRating] = useState(0);
+  const [serviceRating, setServiceRating] = useState(0);
+  const [ambienceRating, setAmbienceRating] = useState(0);
+  const [overallRating, setOverallRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [hover, setHover] = useState(0);
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can handle form submission, e.g., send the data to a backend server
+    console.log({
+      foodRating,
+      serviceRating,
+      ambienceRating,
+      overallRating,
+      comment,
+    });
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-cream text-dark-red">
+      {/* Restaurant information */}
+      <div className="flex gap-x-4 items-center">
+        <Image src={logo} alt="Restaurant Image" width={70} height={70} />
+        <h1 className="text-3xl font-bold">Restaurant Reviews</h1>
+      </div>
+
+      {/* Rating feature */}
+      <div className="grid grid-cols-2 md:gap-4 gap-2 md:w-[45%] w-full lg:px-10 sm:px-4 px-2 justify-center items-center">
+        {/* Food rating */}
+        <span className="text-2xl font-semibold col-span-1">
+          Quality of Food:
+        </span>
+        <div className="flex items-center md:gap-x-2 gap-x-1 col-span-1">
+          <BsEmojiAngry
+            className={`${
+              foodRating == 1
+                ? 'text-[#141414] bg-red-600'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-red-600'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setFoodRating(1)}
+          />
+          <BsEmojiFrown
+            className={`${
+              foodRating == 2
+                ? 'text-[#141414] bg-orange-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-orange-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setFoodRating(2)}
+          />
+          <BsEmojiNeutral
+            className={`${
+              foodRating == 3
+                ? 'text-[#141414] bg-amber-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-amber-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setFoodRating(3)}
+          />
+          <BsEmojiSmile
+            className={`${
+              foodRating == 4
+                ? 'text-[#141414] bg-lime-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-lime-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setFoodRating(4)}
+          />
+          <BsEmojiGrin
+            className={`${
+              foodRating == 5
+                ? 'text-[#141414] bg-green-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-green-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setFoodRating(5)}
+          />
+        </div>
+        {/* Service rating */}
+        <span className="text-2xl font-semibold col-span-1">
+          Quality of Service:
+        </span>
+        <div className="flex items-center md:gap-x-2 gap-x-1 col-span-1">
+          <BsEmojiAngry
+            className={`${
+              serviceRating == 1
+                ? 'text-[#141414] bg-red-600'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-red-600'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setServiceRating(1)}
+          />
+          <BsEmojiFrown
+            className={`${
+              serviceRating == 2
+                ? 'text-[#141414] bg-orange-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-orange-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setServiceRating(2)}
+          />
+          <BsEmojiNeutral
+            className={`${
+              serviceRating == 3
+                ? 'text-[#141414] bg-amber-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-amber-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setServiceRating(3)}
+          />
+          <BsEmojiSmile
+            className={`${
+              serviceRating == 4
+                ? 'text-[#141414] bg-lime-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-lime-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setServiceRating(4)}
+          />
+          <BsEmojiGrin
+            className={`${
+              serviceRating == 5
+                ? 'text-[#141414] bg-green-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-green-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setServiceRating(5)}
+          />
+        </div>
+        {/* Ambience rating */}
+        <span className="text-2xl font-semibold col-span-1">Ambience:</span>
+        <div className="flex items-center md:gap-x-2 gap-x-1 col-span-1">
+          <BsEmojiAngry
+            className={`${
+              ambienceRating == 1
+                ? 'text-[#141414] bg-red-600'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-red-600'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setAmbienceRating(1)}
+          />
+          <BsEmojiFrown
+            className={`${
+              ambienceRating == 2
+                ? 'text-[#141414] bg-orange-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-orange-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setAmbienceRating(2)}
+          />
+          <BsEmojiNeutral
+            className={`${
+              ambienceRating == 3
+                ? 'text-[#141414] bg-amber-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-amber-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setAmbienceRating(3)}
+          />
+          <BsEmojiSmile
+            className={`${
+              ambienceRating == 4
+                ? 'text-[#141414] bg-lime-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-lime-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setAmbienceRating(4)}
+          />
+          <BsEmojiGrin
+            className={`${
+              ambienceRating == 5
+                ? 'text-[#141414] bg-green-500'
+                : 'text-rose hover:text-[#141414] bg-[#EFEFEF] hover:bg-green-500'
+            } w-8 h-8 rounded-full cursor-pointer`}
+            onClick={() => setAmbienceRating(5)}
+          />
+        </div>
+        {/* Overall Rating Stars */}
+        <span className="text-2xl font-semibold col-span-1">
+          Overall Experience:
+        </span>
+        <div className="flex items-center w-full gap-x-3 col-span-1">
+          {[1, 2, 3, 4, 5].map((star) => (
+            <svg
+              key={star}
+              xmlns="http://www.w3.org/2000/svg"
+              className={`${
+                overallRating != 0 && star < overallRating
+                  ? `transition ease-in-out fill-golden`
+                  : 'fill-none'
+              } h-7 w-7 bg-cream text-dark-red hover:fill-golden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 cursor-pointer`}
+              onMouseEnter={() => setOverallRating(star)}
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 2l3.09 6.31L22 9.5l-5 4.87 1.18 6.83L12 18.01l-6.18 3.19L7 14.37 2 9.5l6.91-1.19L12 2z"
+              />
+            </svg>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {/* Comment box */}
+      <div className="flex flex-col items-center gap-y-6 md:w-[75%] w-full lg:px-10 sm:px-4 px-2">
+        <textarea
+          className="w-full h-32 border border-gray-300 rounded p-2"
+          placeholder="Leave a comment..."
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <button className="w-fit px-6 py-1.5 rounded-xl border border-dark-red bg-transparent text-lg font-medium hover:bg-dark-red hover:text-[#EFEFEF]">
+          Submit
+        </button>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        cream: '#F9DEC9',
+        rose: '#F78CA2',
+        'dark-red': '#D80032',
+        brown: '#3D0C11',
+        golden: '#F8DE22',
       },
     },
   },
